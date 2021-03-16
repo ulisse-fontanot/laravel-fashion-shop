813b53ae04b7b85bde0e57330942126f1903c335
ulisse-fontanot <ulissef99@gmail.com>
2021-03-16 17:55:54 +0100

    init-non va pagina

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view('home');
+    }
+
+    public function shop(){
+        return view('shop');
+    }
+
+    public function contatti(){
+        return view('contatti');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title','pagina home')
+
+@section('content')
+    <h1>QUESTA E' LA HOME</h1>
+@endsection
